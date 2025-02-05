7c301822a26d80deb6ff9e08a8f831cb490ab411
Whitehurst <jwhitehurst@kingcounty.gov>
2025-02-05 14:13:33 -0800

    now uses seperate load_df_bcp_f function

--- R/table_duplicate.R
+++ R/table_duplicate.R
@@ -45,6 +45,8 @@
 #'  }
 #'  
 
+devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/apde/main/R/load_df_bcp.R")
+
 table_duplicate_f <- function(conn_from,
                               conn_to,
                               server_to,
@@ -234,7 +236,7 @@ table_duplicate_f <- function(conn_from,
         u <- keyring::key_list(server_to)[["username"]]
         p <- keyring::key_get(server_to, keyring::key_list(server_to)[["username"]])
       }
-      load_bcp_f(dataset = data_from,
+      load_df_bcp_f(dataset = data_from,
                 server = server_to,
                 db_name = db_to,
                 schema_name = table_df[i, "to_schema"],
@@ -369,116 +371,4 @@ table_duplicate_delete_f <- function(conn,
     }
     message("Table(s) deleted.")
   }
-}
-
-# load_bcp_f() ----
-#' @title Load Data to SQL Server Using BCP (Bulk Copy Program)
-#' @description This function loads data into a SQL Server database table using the BCP (Bulk Copy Program) utility.
-#' @author Jeremy Whitehurst, 2024-05-01
-#' 
-#' @param dataset A data.frame or data.table to be loaded, or a character string 
-#' specifying the filepath of a text file to be loaded.
-#' @param server The name of the SQL Server instance.
-#' @param db_name The name of the target database.
-#' @param schema_name The name of the schema containing the target table.
-#' @param table_name The name of the target table.
-#' @param user Optional. The username for SQL Server authentication.
-#' @param pass Optional. The password for SQL Server authentication.
-#'  
-#' @details
-#' This function uses the BCP utility to efficiently load data into a SQL Server 
-#' table. It supports both Windows authentication and SQL Server authentication. 
-#' If a data.frame or data.table is provided, it is first written to a temporary 
-#' file before being loaded.
-#' 
-#' Plans for future improvement ...
-#' 
-#' @importFrom data.table fwrite
-#' @importFrom glue glue
-#' 
-#' @return A character vector containing the output of the BCP command.
-#' 
-#' @export
-#' 
-#' @examples
-#'  \dontrun{
-#'   # Load data from a data.frame
-#'   df <- data.frame(id = 1:5, name = c("Alice", "Bob", "Charlie", "David", "Eve"))
-#'   load_bcp_f(df, "SQLSERVER01", "MyDatabase", "MySchema", "MyTable")
-#'   
-#'   # Load data from a file
-#'   load_bcp_f("C:/data/mydata.txt", "SQLSERVER01", "MySchema", "dbo", "MyTable")
-#'   
-#'   # Load data using SQL Server authentication
-#'   load_bcp_f(df, "SQLSERVER01", "MyDatabase", "dbo", "MyTable", 
-#'             user = "myuser", pass = "mypassword")
-#'  }
-#'
-load_bcp_f <- function(dataset, 
-                       server,
-                       db_name,
-                       schema_name,
-                       table_name, 
-                       user = NULL, 
-                       pass = NULL
-                       ) {
-  
-  if(is.character(dataset)){
-    filepath = dataset
-  }else{
-    filepath = tempfile(fileext = '.txt')
-    on.exit({
-      if(file.exists(filepath)) file.remove(filepath)
-    })
-    data.table::fwrite(dataset, filepath, sep = '\t')
-  }
-  if(!is.null(user) && is.null(pass)) {
-    stop("Must have the pass variable if the user variable is set!")
-  }
-  
-  if(!is.null(user)) {
-    user <- glue::glue("-U {user}")
-    G <- "-G"
-    DT <- "-D"
-  } else {
-    user <- ""
-    G <- ""
-    DT <- "-T"
-  }
-  if(!is.null(pass)) {
-    pass <- glue::glue("-P {pass}")
-  } else {
-    pass <- ""
-  }
-  if(server == "PHClaims") {
-    server <- "KCITSQLPRPENT40"
-  }
-  
-  # Set up BCP arguments and run BCP
-  bcp_args <- c(glue::glue('{schema_name}.{table_name} IN ',
-                     '"{filepath}" ',
-                     '-r \\n ',
-                     '-t \\t ',
-                     '-C 65001 ',
-                     '-F 2 ',
-                     '-S "{server}" ',
-                     '-d {db_name} ', 
-                     '-b 100000 ',
-                     '-c ',
-                     '{G} ',
-                     '{user} ',
-                     '{pass} ',
-                     '-q ',
-                     '{DT}')) 
-  
-  
-  # Load
-  a = system2(command = "bcp", args = c(bcp_args), stdout = TRUE, stderr = TRUE)
-  
-  status = attr(a, 'status')
-  if(length(status)>0 && status == 1){
-    stop(a)
-  }
-  
-  a
 }
